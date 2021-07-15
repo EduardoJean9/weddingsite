0718160709b3cdf0811ed2b6259421cbed094352
EduardoJean <Eduardojean9@gmail.com>
2021-07-15 02:00:26 -0400

    add updated pages and base html

--- jean/views.py
+++ jean/views.py
@@ -3,16 +3,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'jean/home.html')
+    return render(request, 'jean/home.html',{'nbar': 'home'})
 
 def rsvp(request):
-    return render(request,'jean/rsvp.html')
+    return render(request,'jean/rsvp.html', {'nbar': 'rsvp'})
 
 def details(request):
-    return render(request,'jean/details.html')
+    return render(request,'jean/details.html', {'nbar': 'details'})
 
 def honeymoon(request):
-    return render(request,'jean/honeymoon.html')
+    return render(request,'jean/honeymoon.html', {'nbar': 'honeymoon'})
 
 def photos(request):
-    return render(request,'jean/photos.html')
+    return render(request,'jean/photos.html', {'nbar': 'photos'})
